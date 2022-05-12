19aea4e21be458aa5e78454d7536bd847192eecb
juanma <fmjuan89@gmail.com>
2022-05-12 19:25:56 +0100

    Done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="music-box">
+        <div className="lines-box">
+          <div className="central"></div>
+          <div className="uno"></div>
+          <div className="dos"></div>
+          <div className="tres"></div>
+          <div className="cuatro"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
